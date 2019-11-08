be727b7a74d9ebbbc690c1ab749b8418e00ceae5
Bang Wu <ohwubang@gmail.com>
2019-11-08 10:51:04 +0800

    add new command

---
+++ gitfetch.js
@@ -0,0 +1,13 @@
+var shell = require('shelljs')
+shell.exec('pwd')
+shell.ls('./').forEach(item => {
+  console.log('-----------------------------start-----------------------------------')
+  if (shell.cd(`./${item}`).code != 1) {
+    shell.exec('pwd')
+    if (shell.exec('ls -lat | grep .git').code != 1) {
+      shell.exec('git fetch')
+    }
+    shell.cd('..')
+  }
+  console.log('-----------------------------end-----------------------------------')
+})
---
+++ gitpullrebase.js
@@ -0,0 +1,15 @@
+var shell = require('shelljs')
+shell.exec('pwd')
+shell.ls('./').forEach(item => {
+  console.log('-----------------------------start-----------------------------------')
+  if (shell.cd(`./${item}`).code != 1) {
+    shell.exec('pwd')
+    if (shell.exec('ls -lat | grep .git').code != 1) {
+      shell.exec('git stash')
+      shell.exec('git checkout develop')
+      shell.exec('git pull origin develop --rebase')
+    }
+    shell.cd('..')
+  }
+  console.log('-----------------------------end-----------------------------------')
+})
